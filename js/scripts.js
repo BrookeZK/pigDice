e79aa532d092d45136e80e90b36d7bcd29196c03
brooke kullberg and kerri walker <brooke@>
2019-04-10 11:55:32 -0700

    add functions in business logic. In progress.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,41 @@
+//Business Logic for Game
+function Game (players, winners) {
 
-
-
-//Business Logic for Players--------
-function Player (name, age, oneOrTwo, experience, score) {
-  this.name = name,
-  this.age = age,
-  this.oneOrTwo = oneOrTwo,
-  this.experience = experience,
-  this.score = score
 }
 
+Game.prototype.startNewGame = function() {
+  while (Player.id === "player1" && Player.id === "player2") {
+    roll(player1);
+  }
+}
+
+//Business Logic for Players--------
+function Player (name, age, id, experience, score, active) {
+  this.name = name,
+  this.age = age,
+  this.id = id,
+  this.experience = experience,
+  this.score = score,
+  this.active = true
+}
+Player.prototype.addPlayers = function(player) {
+  var gamePlayers = [];
+  gamePlayers.push(player)
+}
+
+Player.prototype.roll = function() {
+  var randomNum = Math.floor(Math.random()* (7-1)+1);
+    if (randomNum===1){
+      this.score +=0;
+      switchPlayer();
+  }
+}
+
+Player.prototype.switchPlayer = function() {
+  player.id
+
+  player.score
+}
 
 //User Interface Logic------------
 
@@ -19,10 +45,12 @@ $(document).ready(function() {
     var inputtedName = $("input#nameInput").val();
     var inputtedAge = $("input#ageInput").val();
     console.log(typeof inputtedAge)
-    var inputtedPlayerNumber = $("input:radio[name=playerOneOrTwo]:checked").val();
+    var inputtedPlayerId = $("input:radio[name=playerId]:checked").val();
     var inputtedExperience = $("input#experienceInput").val();
-    var score = 0
-    var newPlayer = new Player(inputtedName, inputtedAge, inputtedPlayerNumber, inputtedExperience, score);
+    var score = 0;
+    var history = [];
+    var newPlayer = new Player(inputtedName, inputtedAge, inputtedPlayerId, inputtedExperience, score);
+    addPlayers(newPlayer);
     console.log(newPlayer);
     console.log(typeof score);
   })
